ef9e40c63c7a982dc7c6d61b7aa75f8ac3b51283
william-ct <william@clevertap.com>
2021-04-16 14:32:29 +0530

    added firebase messaging deps

--- app/src/main/java/com/carlosmesquita/technicaltest/n26/bitlue/BitlueApplication.kt
+++ app/src/main/java/com/carlosmesquita/technicaltest/n26/bitlue/BitlueApplication.kt
@@ -1,7 +1,11 @@
 package com.carlosmesquita.technicaltest.n26.bitlue
 
 import android.app.Application
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -14,6 +18,21 @@ class BitlueApplication : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("App", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = ""+ token
+            Log.d("App", msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
     }
